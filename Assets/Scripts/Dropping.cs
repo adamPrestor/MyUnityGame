d29e817df5c2780c1774c4e6ff1a8b96df797662
adamPrestor <adam.prestor@gmail.com>
2017-05-04 10:16:12 +0200

    pismo srbov - oddano v ocenjevanje
    dodane scene - martinov kviz, pismo srbov, darwin dialog

---
+++ Assets/Scripts/Dropping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropping : MonoBehaviour, IDropHandler
+{
+
+    public bool inventory = false;
+
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+
+        if(d != null)
+        {
+            if(this.transform.childCount == 0 || inventory)
+            {
+                d.passedParent = this.transform;
+            }
+        }
+
+    }
+
+}
+    
---
+++ Assets/Scripts/MartinQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MartinQuestion {
+
+    public string Question;
+    public string AnswerA;
+    public string AnswerB;
+    public string AnswerC;
+    public int truth;
+
+}
